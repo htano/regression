77f24d046d3e32897444a39bf0c15b23ea2e2456
htano <htano.dev@gmail.com>
2017-02-23 22:30:19 +0900

    リファクタリング。

--- regression.py
+++ regression.py
@@ -16,14 +16,21 @@ if __name__ == '__main__':
     sales_num   = [77, 62, 93, 84, 59, 64, 80, 75, 58, 91, 51, 73, 65, 84]
 
     # 平均 average
-    temp_ave = get_average(temperature)
+    # 気温の平均 : temp_ave = x, 注文数の平均 : sales_num = y
+    temp_ave   = get_average(temperature)
     sales_ave  = get_average(sales_num)
 
-    # 共分散 covariance
-    deviation_sum = 0
+    print ('temperature averag    : ' + str(temp_ave))
+    print ('sales average          : ' + str(sales_ave))
+
+    # 偏差積和 sum of products of deviation : Sxy
+    Sxy = 0
     for i in range(len(temperature)):
-        deviation_sum += (temperature[i] - temp_ave) * (sales_num[i] - sales_ave)
-    covariance = deviation_sum / len(temperature)
+        Sxy += (temperature[i] - temp_ave) * (sales_num[i] - sales_ave)
+    # 共分散 covariance
+    covariance = Sxy / len(temperature)
+
+    print ('covariance             : ' + str(covariance))
 
     # 標準偏差 standard deviation
     temp_standard_deviation = np.std(temperature)
@@ -32,47 +39,66 @@ if __name__ == '__main__':
     # 単相関係数 correlation coefficient
     correlation_coefficient = covariance / (temp_standard_deviation * sales_standard_deviation)
 
+    print ('correlation coeficient : ' + str(correlation_coefficient))
+
     # 偏差平方和 sum of square deviation
-    # Sxx : temp_sum_of_square_deviation, Syy : sales_sum_of_square_deviation
-    temp_sum_of_square_deviation = calc_sum_of_square_deviation(temperature, temp_ave)
-    sales_sum_of_square_deviation = calc_sum_of_square_deviation(sales_num, sales_ave)
-    # 偏差積和 sum of products of deviation
-    # Sxy : deviation_sum
+    # 気温の偏差平方和 : Sxx, 注文数の偏差平方和 : Syy
+    Sxx = calc_sum_of_square_deviation(temperature, temp_ave)
+    Syy = calc_sum_of_square_deviation(sales_num, sales_ave)
+
+    print ('Sxx : ' + str(Sxx))
+    print ('Syy : ' + str(Syy))
+    print ('Sxy : ' + str(Sxy))
 
     # a = Sxy / Sxx, b = ave_y - ave_x * a
-    a = deviation_sum / temp_sum_of_square_deviation
+    a = Sxy / Sxx
     b = sales_ave - temp_ave * a
+
+    print ('a : ' + str(a))
+    print ('b : ' + str(b))
 
     predict_sales_num = []
     for i in range(len(temperature)):
         predict_sales_num.append(a * temperature[i] + b)
+
+    # 予想値 : y
     predict_sales_ave = get_average(predict_sales_num)
 
+    print ('predict sales ave : ' + str(predict_sales_ave))
+
     # 偏差平方和 sum of square deviation
-    # Sy^y^
-    predict_sales_sum_of_square_deviation = \
-        calc_sum_of_square_deviation(predict_sales_num, predict_sales_ave)
+    # Sy^y^ : Sy_y_
+    Sy_y_ = calc_sum_of_square_deviation(predict_sales_num, predict_sales_ave)
+
+    print ('Sy^y^  : ' + str(Sy_y_))
 
     # 偏差積和 sum of products of deviation
-    # Syy^ : Sy-y_
-    Sy_y_ = 0
+    # Syy^ : Syy_
+    Syy_ = 0
     for i in range(len(sales_num)):
-        Sy_y_ += (sales_num[i] - sales_ave) * (predict_sales_num[i] - predict_sales_ave)
+        Syy_ += (sales_num[i] - sales_ave) * (predict_sales_num[i] - predict_sales_ave)
+
+    print ('Syy^ : ' + str(Syy_))
 
     Se = 0
     for i in range(len(sales_num)):
         Se += (sales_num[i] - predict_sales_num[i]) ** 2
 
+    print ('Se : ' + str(Se))
+
     # 重相関係数 multiple correlation coefficient
     # R = Syy^ / sqrt(Syy * Sy^y^)
-    R = Sy_y_ / math.sqrt(sales_sum_of_square_deviation * predict_sales_sum_of_square_deviation)
+    R = Syy_ / math.sqrt(Syy * Sy_y_)
     # 寄与率 contribution ratio
     RR = R ** 2
 
+    print ('R    : ' + str(R))
+    print ('R**2 : ' + str(RR))
+
     # RR = (a * Sxy)/Syy
-    #print ((a * deviation_sum)/sales_sum_of_square_deviation)
+    #print ((a * Sxy)/Syy)
     # RR = (1- Se/Syy)
-    #print (1 - (Se/sales_sum_of_square_deviation))
+    #print (1 - (Se/Syy))
 
     # 回帰係数の検定
     # 母回帰 y = Ax + B
@@ -80,25 +106,33 @@ if __name__ == '__main__':
     B = b
     sigma = math.sqrt(Se/(len(temperature)-2))
 
+    print ('A     : ' + str(A))
+    print ('B     : ' + str(B))
+    print ('sigma : ' + str(sigma))
     # 有意水準 significance level
     significance_level = 0.05
 
     # 回帰係数の検定
     approval_val = \
-        (a ** 2)/(1/temp_sum_of_square_deviation) / (Se/(len(temperature) - 2))
+        (a ** 2)/(1/Sxx) / (Se/(len(temperature) - 2))
+
+    print ('approval val : ' + str(approval_val))
 
     # F分布からPを求める
     # 保留
 
     # 気温 31度 信頼率 95% における信頼区間の算出
     # F(1, 14-2;0.05) = 4.7
-    section = math.sqrt(4.7 * \
-        ((1/len(temperature) + (31 - temp_ave)**2 / temp_sum_of_square_deviation) * \
+    trusted_section = math.sqrt(4.7 * \
+        ((1/len(temperature) + (31 - temp_ave)**2 / Sxx) * \
         Se/(len(temperature) - 2 )))
+
+    print ('trusted section : ' + str(trusted_section))
 
     # 推定
     # 気温 27度 信頼率 95% における予測区間の算出
     # F(1, 14-2;0.05) = 4.7
     predicted_section = math.sqrt(4.7 * \
-        (1 + 1/len(temperature) + (27 - temp_ave) ** 2 / temp_sum_of_square_deviation) * \
+        (1 + 1/len(temperature) + (27 - temp_ave) ** 2 / Sxx) * \
         Se / (len(temperature) - 2))
+    print ('predicted section : ' + str(predicted_section))
